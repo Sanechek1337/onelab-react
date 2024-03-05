39c71578010119131d3059a19700846b931e041a
Alexander <rednaxela659@gmail.com>
2024-03-05 16:49:42 +0500

    feat: add styles for person list, implement remove person logic

--- src/page/List.jsx
+++ src/page/List.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import Person from "../components/Person";
 
 export default function List() {
@@ -14,11 +15,45 @@ export default function List() {
     setData(list);
   }, []);
 
+  if (data.length <= 0) return <p>Нет данных</p>;
+
   return (
-    <div>
-      {data.map((person) => (
-        <Person person={person} />
-      ))}
-    </div>
+    <StyledTable>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Phone number</th>
+          <th>Delete user</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((person, index) => (
+          <Person person={person} key={index} data={data} setData={setData} />
+        ))}
+      </tbody>
+    </StyledTable>
   );
 }
+
+const StyledTable = styled("table")`
+  border-collapse: collapse;
+  background-color: #fff;
+  text-align: center;
+
+  td,
+  th {
+    border: 1px solid #000;
+    padding: 10px 20px;
+  }
+
+  thead {
+    background-color: #455491;
+    color: #fff;
+  }
+
+  tr:nth-child(even) {
+    background-color: #dddddd;
+  }
+`;
