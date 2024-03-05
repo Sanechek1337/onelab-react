a563a2fc8fb6011ee86fbc7c01ec494831ff8805
Alexander <rednaxela659@gmail.com>
2024-03-05 15:06:21 +0500

    feat: add styles for add user page

--- src/page/Add.jsx
+++ src/page/Add.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function Add() {
   const [name, setName] = useState("");
@@ -21,7 +22,6 @@ export default function Add() {
   };
 
   const onSave = () => {
-    if (!name || !surname || !phone) return;
     let list = sessionStorage.getItem("list");
     if (!list) {
       list = [];
@@ -34,35 +34,80 @@ export default function Add() {
       phone,
     });
     sessionStorage.setItem("list", JSON.stringify(list));
+    alert(`Пользователь ${name} добавлен`);
     navigateTo("/");
   };
 
   return (
-    <div className="form">
-      <div className="form-content">
-        <input
+    <StyledFormContainer>
+      <StyledForm>
+        <StyledInput
           value={name}
           onChange={handleNameChange}
           type="text"
           name="name"
           placeholder="First Name"
         />
-        <input
+        <StyledInput
           value={surname}
           onChange={handleSurnameChange}
           type="text"
           name="surname"
           placeholder="Last Name"
         />
-        <input
+        <StyledInput
           value={phone}
           onChange={handlePhoneChange}
           type="text"
           name="phonenumber"
           placeholder="Phone Number"
         />
-        <button onClick={onSave}>Save</button>
-      </div>
-    </div>
+        <StyledButton onClick={onSave} disabled={!name || !surname || !phone}>
+          Save
+        </StyledButton>
+      </StyledForm>
+    </StyledFormContainer>
   );
 }
+
+const StyledFormContainer = styled("div")`
+  width: 350px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #00000038;
+  border: 3px solid #2a3357;
+  border-radius: 12px;
+`;
+
+const StyledForm = styled("div")`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const StyledInput = styled("input")`
+  padding: 6px 12px;
+  border-radius: 8px;
+  border: 2px solid #808080;
+  font-size: 18px;
+`;
+
+const StyledButton = styled("button")`
+  align-self: center;
+  padding: 8px 20px;
+  background-color: #40486d;
+  color: #fff;
+  font-size: 20px;
+  border-radius: 8px;
+  border: none;
+
+  &:hover {
+    background-color: #2a3357;
+  }
+
+  &:disabled {
+    background-color: #d5d1d2;
+  }
+`;
