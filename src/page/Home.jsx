3dbbdb6fdbc8249a796d61f69c33069a3590c2a7
Alexandr <rednaxela659@gmail.com>
2024-03-09 20:46:37 +0500

    feat: change styles for navigation buttons

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -20,18 +20,35 @@ const StyledNav = styled('nav')`
 const StyledLink = styled(Link)`
   width: 150px;
   height: 100px;
+  text-decoration: none;
+  text-transform: uppercase;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 8px;
-  background-color: #9e9e9e;
-  color: #fff;
-  text-decoration: none;
   text-align: center;
+  font-weight: 700;
+  padding: 8px 40px;
+  background-color: transparent;
+  border: 1px solid #03e9f4;
+  color: #03e9f4;
+  letter-spacing: 1px;
   font-size: 20px;
-  font-weight: 500;
-  padding: 0 10px;
+  border-radius: 8px;
+  transition: 0.5s;
+
   &:hover {
-    background-color: #8f8f8f;
+    background-color: #01a9b2;
+    color: #fff;
+    border-radius: 5px;
+    box-shadow:
+      0 0 5px #01a9b2,
+      0 0 25px #01a9b2;
+  }
+
+  &:disabled {
+    background-color: #d5d1d2;
+    border: 1px solid transparent;
+    color: #343434;
+    box-shadow: none;
   }
 `;
